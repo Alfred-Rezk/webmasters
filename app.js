7d75828e3813a1ccb03d3f44fe862af9aca56d23
Scooter84 <alfred.rezk@yahoo.com>
2018-10-30 11:26:29 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var aboutRouter = require('./routes/about');
 var servicesRouter = require('./routes/services');
+var contactRouter = require('./routes/contact');
 var app = express();
 
 // view engine setup
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
 app.use('/services', servicesRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
